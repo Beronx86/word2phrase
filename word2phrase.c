429b47da7d4f0dfa406a999fe392f78e50bf5b52
Beronx86 <laupeng1989@gmail.com>
2015-01-26 22:13:05 +0800

    master a test string

--- word2phrase.c
+++ word2phrase.c
@@ -11,7 +11,6 @@
 //  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
-//  test string to see the branch
 
 #include <stdio.h>
 #include <stdlib.h>
